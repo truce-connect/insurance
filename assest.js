fcdf2b2b6ffa546aae1dd0bd6893bada6a51bdec
Tayo oyebanji <100080174+truce-connect@users.noreply.github.com>
2023-05-04 02:09:39 +0100

    Add files via upload

---
+++ assest.js
@@ -0,0 +1,41 @@
+const orders =[
+  {
+    productName :'foldable mini drone',
+    productNumber :8531,
+    paymentstatus : 'due',
+     shipping:'pending '
+  },
+  {
+    productName :'foldable mini drone',
+    productNumber :8531,
+    paymentstatus : 'due',
+     shipping:'pending '
+  },
+{
+    productName :'foldable mini drone',
+    productNumber :8531,
+    paymentstatus : 'due',
+     shipping:'pending'
+   
+
+},
+{
+    productName :'corper drone',
+    productNumber :8531,
+    paymentstatus :' due',
+     shipping:'pending'
+},
+
+{
+    productName :'leverender',
+    productNumber :8531,
+    paymentstatus : 'due',
+     shipping:'pending'
+
+
+}
+
+
+
+
+]
